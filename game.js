ff47ae12ad5cd9801b3e31513495dce469ee88af
carlosagpjr <carlosagpjr@gmail.com>
2025-05-09 16:11:39 -0300

    Update teste map game.js

--- game.js
+++ game.js
@@ -1,27 +1,6 @@
-class MainScene extends Phaser.Scene {
-  constructor() {
-    super('MainScene');
-    this.weaponType = 'range';
-  }
-
-  preload() {
-    this.load.image('player', './assets/player-icon.png');
-    this.load.image('crawler', './assets/crawler-icon.png');
-    this.load.image('chaser', './assets/chaser-icon.png');
-    this.load.image('bouncer', './assets/bouncer-icon.png');
-    this.load.image('leaper', './assets/leaper-icon.png');
-    this.load.image('exploder', './assets/exploder-icon.png');
-    this.load.image('bruiser', './assets/bruiser-icon.png');
-    this.load.image('reaper', './assets/reaper-icon.png');
-    this.load.image('overlord', './assets/overlord-icon.png');
-    this.load.image('bullet', 'https://labs.phaser.io/assets/sprites/bullets/bullet11.png');
-    this.load.image('background', './assets/wasteland-background.png');
-  }
-
-  create() {
-    const mapWidth = 5000;
-    const mapHeight = 5000;
-    this.background = this.add.tileSprite(0, 0, mapWidth, mapHeight, 'background').setOrigin(0);
+this.background = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, 'background')
+      .setScrollFactor(0)
+      .setOrigin(0);
     this.cameras.main.setBounds(0, 0, mapWidth, mapHeight);
     this.physics.world.setBounds(0, 0, mapWidth, mapHeight);
 
@@ -212,8 +191,8 @@ class MainScene extends Phaser.Scene {
 
 const config = {
   type: Phaser.AUTO,
-  width: window.innerWidth,
-  height: window.innerHeight,
+  width: 800,
+  height: 600,
   backgroundColor: '#222222',
   physics: {
     default: 'arcade',
@@ -224,8 +203,4 @@ const config = {
   scene: MainScene
 };
 
-const game = new Phaser.Game(config);
-
-window.addEventListener('resize', () => {
-  game.scale.resize(window.innerWidth, window.innerHeight);
-});
+new Phaser.Game(config);
